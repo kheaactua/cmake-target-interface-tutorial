ba08315e8da66eab3a3e8418def999644cceac1d
Matthew Russell <mrussell@neptec.com>
2018-01-10 09:53:04 -0500

    Added a proper cmake example

---
+++ example-03-cmake/FindScripts/FindExtra.cmake
@@ -0,0 +1,42 @@
+include(FindPackageHandleStandardArgs)
+
+# Find paths to assimp folders
+# Inlcudes path
+find_path(EXTRA_LIB_BASE_DIR
+	NAMES include/extra/extra_functions.h
+	PATHS
+		${VENDOR_DIR}/extra
+	NO_DEFAULT_PATH
+)
+
+find_library(EXTRA_LIB_LIB
+	NAMES extra
+	PATHS
+		${EXTRA_LIB_BASE_DIR}
+	NO_DEFAULT_PATH
+)
+
+find_path(EXTRA_LIB_INCLUDE_DIR
+	NAMES extra/extra_functions.h
+	PATHS
+		${EXTRA_LIB_BASE_DIR}/include
+	NO_DEFAULT_PATH
+)
+
+if (NOT TARGET Extra)
+	message("Extra Lib")
+	message("  Include: ${EXTRA_LIB_INCLUDE_DIR}")
+	message("  Lib:     ${EXTRA_LIB_LIB}")
+
+	if (WIN32)
+			MESSAGE(STATUS "   Bin: ${EXTRA_LIB_BASE_DIR}/bin")
+	endif()
+
+	add_library(Extra INTERFACE)
+	set_property(TARGET Extra PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${EXTRA_LIB_INCLUDE_DIR})
+	set_property(TARGET Extra PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${EXTRA_LIB_INCLUDE_DIR})
+
+	target_link_libraries(Extra INTERFACE ${EXTRA_LIB_LIB})
+endif()
+
+# vim: ts=2 sw=2 sts=2 noet ffs=unix ft=cmake :
